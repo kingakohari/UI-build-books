cae9e2ec3c502a6880d92fd8d93d6226fbe7f4eb
Kinga Kohari <kinguca@yahoo.com>
2022-05-18 11:11:16 +0200

    Setup done

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(`${__dirname}/../frontend/index.html`)) 
+})
+
+app.use("/public", express.static(`${__dirname}/../frontend/public`))
+
+app.listen(9000, () => {
+	console.log("http://127.0.0.1:9000");
+})
